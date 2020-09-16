9ff69137e0d64021b877fcb6b09cb5118f1751fb
rice-y <ykmj00873@gmail.com>
2020-09-16 21:31:02 +0900

    目標フォーム、コントローラー

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -2,4 +2,32 @@ class TargetsController < ApplicationController
   def index
     
   end
+<<<<<<< Updated upstream
+=======
+  def create
+    @target = Target.new(target_params)
+      if @target.save
+        redirect_to root_path
+      else
+        render :new
+      end 
+   end
+
+
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+
+  private
+
+  def target_params
+    params.permit(:image, :name, :text, :price, :deadline)
+  end
+
+  
+>>>>>>> Stashed changes
 end
